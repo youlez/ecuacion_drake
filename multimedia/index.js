25c723999d0b1c129ff707ed7d157fb833fb8af2
Youlez <karent.pinzon@idartes.gov.co>
2024-02-07 00:03:11 -0500

    se agrega los radiotelescopios

---
+++ multimedia/index.js
@@ -0,0 +1,43 @@
+const {app, BrowserWindow, ipcMain} = require('electron');
+const url = require('url');
+const path = require('path');
+let mainWindow;
+let secondWindow;
+app.on('ready',()=>{
+    mainWindow = new BrowserWindow({
+        /*fullscreen: true,
+        autoHideMenuBar: true,*/
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    });
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname,'view/primera_pantalla/index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+    mainWindow.on("closed", () => {app.quit();})
+
+    secondWindow = new BrowserWindow({
+        /*fullscreen: true,
+        autoHideMenuBar: true,*/
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    });
+    secondWindow.loadURL(url.format({
+        pathname: path.join(__dirname,'view/segunda_pantalla/index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+    secondWindow.on("closed", () => {app.quit();})
+
+    ipcMain.on('pagina', (e, pagina) => {
+        secondWindow.webContents.send('pagina',pagina);
+    })
+    ipcMain.on('valores', (e, valores) => {
+        secondWindow.webContents.send('valores',valores);
+    })
+})
